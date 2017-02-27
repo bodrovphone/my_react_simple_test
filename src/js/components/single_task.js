7829906fcdd7a4f8447ae417fb4fa5ac2e9c8a28
obodrov <bodrovphone@gmail.com>
2017-02-27 21:26:13 +0200

    1.2

--- src/js/components/single_task.js
+++ src/js/components/single_task.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 export default class SingleTask extends Component {
     render() {
     return (
-        <div className="row">
-            <input type="checkbox" name="isDone"/>
-            <label htmlFor="isDone">{this.props.name}</label>
+        <div className="row single-task">
+            <input className="checkbox" type="checkbox" name="isDone" id="checkbox" />
+            <label htmlFor="checkbox">{this.props.name}</label>
         </div>
     );   }
 }
