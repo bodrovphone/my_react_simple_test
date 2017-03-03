307a1c322f3cc8590e399cc49e87a7121eaa5b62
obodrov <bodrovphone@gmail.com>
2017-03-03 21:47:16 +0200

    1.7

--- src/js/components/current_tasks.js
+++ src/js/components/current_tasks.js
@@ -3,23 +3,23 @@ import React, { Component } from 'react';
 import SingleTask from './single_task';
 
 export default class CurrentTasks extends Component {
- constructor(props) {
-     super(props);
-     this.toggleTask = this.toggleTask.bind(this);
- }
- toggleTask(event) {
-    this.props.updateTasks(event);
- }
- render () {
-    const allTasks = this.props.allTasks;
-    if (!allTasks.length) return null;
-        return (
-            <div className="container task-list">
-                {allTasks.map((task,index) => {
-                    return <SingleTask isDone={task.done} key={index} index={index} name={task.name} handleInputChange={this.toggleTask}>{task.name}</SingleTask>;
-                })}
-                <footer></footer>
-            </div>
-        );
- }   
+    constructor(props) {
+         super(props);
+         this.toggleTask = this.toggleTask.bind(this);
+    }
+    toggleTask(event) {
+        this.props.updateTasks(event);
+    }
+    render () {
+       const allTasks = this.props.allTasks;
+        if (!allTasks.length) return null;
+            return (
+                <div className="container task-list">
+                    {allTasks.map((task,index) => {
+                        return <SingleTask isDone={task.done} key={index} index={index} name={task.name} handleInputChange={this.toggleTask}>{task.name}</SingleTask>;
+                    })}
+                    <footer></footer>
+                </div>
+            );
+    }
 }
--- src/js/components/single_task.js
+++ src/js/components/single_task.js
@@ -5,23 +5,23 @@ export default class SingleTask extends Component {
         super(props);
         this.handleOnMouseOver = this.handleOnMouseOver.bind(this);
         this.handleOffMouseOver = this.handleOffMouseOver.bind(this);
+        this.hura = this.hura.bind(this);
     }
     handleOnMouseOver(event) {
-        // getElementByid(event.target);
-        // event.target.lastChild.className = "delete delete-hover";
-        console.log(event.target);
+        event.currentTarget.lastChild.className = "delete delete-hover";
     }
     handleOffMouseOver(event) {
-        // getElementByid(event.target);
-        // event.target.lastChild.className = "delete";
-        console.log(event.target);
+        event.currentTarget.lastChild.className = "delete";
+    }
+    hura() {
+        alert('hura');
     }
     render() {
     return (
         <div onMouseEnter={this.handleOnMouseOver} onMouseLeave={this.handleOffMouseOver} className="row single-task">
             <input className="checkbox" type="checkbox" id={this.props.index} name={this.props.name} onChange={this.props.handleInputChange} checked={this.props.isDone} />
             <label className={this.props.isDone ? 'task-is-done' : ''}>{this.props.name}</label>
-            <button className="delete"></button>
+            <button className="delete" onClick={this.hura}></button>
         </div>
     );   }
 }
